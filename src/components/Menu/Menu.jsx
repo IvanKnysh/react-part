228285744feec0b340c7ee94331b532a3a0ab4b8
Ivan Knysh <mr.rauber.ik@gmail.com>
2024-03-26 15:59:09 +0200

    ДЗ 45. Проект: useEffect для запитів

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import MenuItem from "./MenuItem";
-import pizzas from "../../../data";
 
 const Menu = () => {
+	const [pizzas, setPizzas] = useState([]);
+	const [error, setError] = useState("");
+
+	useEffect(() => {
+		const getAllPizzas = async () => {
+			try {
+				const response = await fetch(
+					"https://react-fast-pizza-api.onrender.com/api/menu"
+				);
+
+				if (!response.ok) {
+					throw new Error("Failed to fetch");
+				}
+
+				const { data } = await response.json();
+				setPizzas(data);
+			} catch (err) {
+				setError(err.message);
+			}
+		};
+
+		getAllPizzas();
+	}, []);
+
 	return (
 		<ul>
+			{error && <p>{error}</p>}
 			{pizzas.map((item) => (
 				<MenuItem key={item.id} item={item} />
 			))}
