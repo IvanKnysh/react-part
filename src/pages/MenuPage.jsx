b07ad43d2ad655c5fe0d1243a875a67a915b4a15
Ivan Knysh <mr.rauber.ik@gmail.com>
2024-03-27 12:20:11 +0200

    ДЗ 45. Проект: useEffect для запитів -> перездача

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,10 +1,44 @@
-import Menu from "../components/Menu/Menu";
+import { useEffect, useState } from "react";
+import MenuItem from "../components/MenuItem/MenuItem";
 
 const MenuPage = () => {
+	const [pizzas, setPizzas] = useState([]);
+	const [error, setError] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		const getAllPizzas = async () => {
+			setIsLoading(true);
+
+			try {
+				const response = await fetch(
+					"https://react-fast-pizza-api.onrender.com/api/menu"
+				);
+
+				if (!response.ok) {
+					throw new Error("Failed to fetch");
+				}
+
+				const { data } = await response.json();
+				setPizzas(data);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		getAllPizzas();
+	}, []);
+
 	return (
-		<>
-			<Menu />
-		</>
+		<ul className="menu">
+			{error && <p>{error}</p>}
+			{isLoading && <p>Loading...</p>}
+			{pizzas.map((item) => (
+				<MenuItem key={item.id} item={item} />
+			))}
+		</ul>
 	);
 };
 
