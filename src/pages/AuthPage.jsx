69fe154129db38757f0b5be3757e4eef24e51a96
Ivan Knysh <mr.rauber.ik@gmail.com>
2024-04-05 16:13:47 +0300

    ДЗ 47. Проект: практика з Context

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,16 +1,33 @@
-import Button from "../components/Button/Button";
-import Form from "../components/Form/Form";
-import Input from "../components/Input/Input";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
 
 const AuthPage = () => {
+	const { setUser } = useContext(AuthContext);
+	const [userInput, setUserInput] = useState("");
+
+	const navigate = useNavigate();
+
+	const formHandler = (e) => {
+		e.preventDefault();
+
+		setUser(userInput);
+		navigate("/menu");
+	};
+
 	return (
 		<>
 			<h1 className="title">Auth</h1>
 
-			<Form>
-				<Input type="text" placeholder="Your email" />
-				<Button text="Login" />
-			</Form>
+			<form onSubmit={formHandler} className="login-form">
+				<input
+					type="text"
+					value={userInput}
+					onChange={(e) => setUserInput(e.target.value)}
+					placeholder="Your name"
+				/>
+				<button>Send</button>
+			</form>
 		</>
 	);
 };
